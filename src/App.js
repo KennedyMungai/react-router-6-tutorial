100c57e3f00359d7e2a1152b2d2dc541d9b77f73
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 17:25:02 +0300

    Created an index route and directed ot to the Home component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App()
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route index element={<Home />}>
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="*" element={<Error />} />
