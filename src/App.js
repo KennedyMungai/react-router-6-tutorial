3fde17ce981155508c70b4a6379d3888cf452689
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 15:03:34 +0300

    Added a route to handle all unexpected routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App()
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
