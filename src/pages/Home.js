68ad377b4e72c80b302c7951bde9825cf3a8cae0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 15:00:21 +0300

    Created a link to the about page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ const Home = () =>
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <Link to='/about' className="btn">
+        About
+      </Link>
     </section>
   );
 };
