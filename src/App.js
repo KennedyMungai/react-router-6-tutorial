1543af7787e185ea36e87bbd73c5a4cbe2400f21
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 17:36:05 +0300

    Deleted the Dashboard import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
-import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
