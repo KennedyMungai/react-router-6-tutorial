5b073de6e823e233820868897f5929bde37b13ae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 14:38:50 +0300

    Imported the Home component and set its path to root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Home from "./pages/Home";
 
 function App()
 {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ } />
+        <Route path="/" element={<Home />} />
         <Route path="/testing" element={ } />
       </Routes>
     </Router>
