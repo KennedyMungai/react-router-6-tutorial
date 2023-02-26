8ef50a4b637a84ed7bcbebdb80089d03c2f1684a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 17:42:21 +0300

    Used Styled Navbar instead of Navbar

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import StyledNavbar from "../components/StyledNavbar";
 
 const SharedLayout = () =>
 {
     return (
         <>
-            <NavBar />
+            {/* <NavBar /> */}
+            <StyledNavbar />
             <section className='section'>
                 <Outlet />
             </section>
