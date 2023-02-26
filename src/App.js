30aaf0a1ad02264c9cf7e8b7f9a5dab73e842958
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 18:19:01 +0300

    Imported the Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
