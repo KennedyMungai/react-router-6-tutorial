9ccda589872a73e2a8c6da6206da47191d226099
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 17:57:25 +0300

    Instantiated the useParams hook

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 
 const SingleProduct = () =>
 {
+  const params = useParams()
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>{ }</h2>
     </section>
   );
 };
