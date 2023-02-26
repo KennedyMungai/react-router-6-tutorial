784506e82a72aaaf164051a0470f49187fddcb40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 17:41:37 +0300

    Used NavLinks instead of Links

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -4,7 +4,11 @@ import { NavLink } from 'react-router-dom'
 const StyledNavbar = () =>
 {
     return (
-        <div>StyledNavbar</div>
+        <nav className="navbar">
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+            <NavLink to='/products'>Products</NavLink>
+        </nav>
     )
 }
 
