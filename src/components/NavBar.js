508db7d98e4eb619dbda156601df9a2c50b33275
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-26 17:17:57 +0300

    Created three Links, for Home About and Products

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom'
 const NavBar = () =>
 {
     return (
-        <div>NavBar</div>
+        <nav className='navbar'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/products'>Products</Link>
+        </nav>
     )
 }
 
